cac36ce0b25ee0505210e0651ecc9b45a579f04a
Alexandre Bergeron <alex@waj.am>
2013-09-19 16:47:23 -0400

    Added Logging trait to commons-core

---
+++ commons-core/src/main/scala/com/wajam/commons/Logging.scala
@@ -0,0 +1,46 @@
+package com.wajam.commons
+
+import org.slf4j.LoggerFactory
+
+/**
+ * Trait that add logging capability to a class
+ */
+trait Logging {
+  val log = LoggerFactory.getLogger(getClass)
+
+  def debug(msg: => String, params: Any*) {
+    if (log.isDebugEnabled) {
+      val arr = params.map(m => m.asInstanceOf[Object]).toArray
+      log.debug(msg, arr)
+    }
+  }
+
+  def trace(msg: => String, params: Any*) {
+    if (log.isTraceEnabled) {
+      val arr = params.map(m => m.asInstanceOf[Object]).toArray
+      log.trace(msg, arr)
+    }
+  }
+
+  def info(msg: => String, params: Any*) {
+    if (log.isInfoEnabled) {
+      val arr = params.map(m => m.asInstanceOf[Object]).toArray
+      log.info(msg, arr)
+    }
+  }
+
+  def warn(msg: => String, params: Any*) {
+    if (log.isWarnEnabled) {
+      val arr = params.map(m => m.asInstanceOf[Object]).toArray
+      log.warn(msg, arr)
+    }
+  }
+
+  def error(msg: => String, params: Any*) {
+    if (log.isErrorEnabled) {
+      val arr = params.map(m => m.asInstanceOf[Object]).toArray
+      log.error(msg, arr)
+    }
+  }
+
+}
