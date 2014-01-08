e09eb06f7855796a2651edc3d4781c085f8ef28a
mmimeault <michael@wajam.com>
2014-01-08 13:56:55 -0500

    Forgot to change them in test file.(usage)

--- commons-gearman/src/it/scala/com/wajam/gearman/TestGearmanClient.scala
+++ commons-gearman/src/it/scala/com/wajam/gearman/TestGearmanClient.scala
@@ -17,7 +17,7 @@ class TestGearmanClient extends GearmanIntegrationTest {
 
   "Working client" should "not fail when executeJob success data" in {
     val data = Map("success" -> "success")
-    val f = client.executeJob(TEST_FUNCTION_NAME, data)
+    val f = client.executeJob(testFunctionName, data)
     f.onComplete {
       case Success(v) => data should equal(v)
       case Failure(e) => fail("Success data should return a success")
@@ -27,13 +27,13 @@ class TestGearmanClient extends GearmanIntegrationTest {
 
   "Working client" should "fail when executeJob fail data" in {
     val data = Map("fail" -> "fail")
-    val f = client.executeJob(TEST_FUNCTION_NAME, data)
+    val f = client.executeJob(testFunctionName, data)
     evaluating(Await.result(f, 2.seconds)) should produce[JobExecutionException]
   }
 
   "Working client" should "not fail when enqueueJob success data" in {
     val data = Map("success" -> "success")
-    val f = client.enqueueJob(TEST_FUNCTION_NAME, data)
+    val f = client.enqueueJob(testFunctionName, data)
     f.onComplete {
       case Success(v) => data should equal(v)
       case Failure(e) => fail("Success data should return a success")
@@ -43,7 +43,7 @@ class TestGearmanClient extends GearmanIntegrationTest {
 
   "Working client" should "not fail when enqueueJob fail data" in {
     val data = Map("fail" -> "fail")
-    val f = client.enqueueJob(TEST_FUNCTION_NAME, data)
+    val f = client.enqueueJob(testFunctionName, data)
     f.onComplete {
       case Success(v) => data should equal(v)
       case Failure(e) => fail("Success data should return a success")
@@ -54,13 +54,13 @@ class TestGearmanClient extends GearmanIntegrationTest {
   "Non working client" should "throw exception on first queuing/execute and then queue all others without executing them" in {
     val data = Map("success" -> "success")
 
-    val f1 = notWorkingClient.executeJob(TEST_FUNCTION_NAME, data)
+    val f1 = notWorkingClient.executeJob(testFunctionName, data)
     evaluating(Await.result(f1, 2.seconds)) should produce[JobSubmissionException]
 
-    val f2 = notWorkingClient.executeJob(TEST_FUNCTION_NAME, data)
+    val f2 = notWorkingClient.executeJob(testFunctionName, data)
     evaluating(Await.ready(f2, 2.seconds)) should produce[TimeoutException]
 
-    val f3 = notWorkingClient.enqueueJob(TEST_FUNCTION_NAME, data)
+    val f3 = notWorkingClient.enqueueJob(testFunctionName, data)
     evaluating(Await.ready(f3, 2.seconds)) should produce[TimeoutException]
   }
 
