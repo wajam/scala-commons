1387893389d008f725b7531f30a9ddfaee444c2d
Manuel Laflamme <manuel@waj.am>
2014-01-10 13:27:19 -0500

    Tracing execution context
    Execution context decorator that saves the current tracing context when created and restore it every time it is executed.

---
+++ commons-tracing/src/main/scala/com/wajam/tracing/TracingExecutionContext.scala
@@ -0,0 +1,27 @@
+package com.wajam.tracing
+
+import scala.concurrent.ExecutionContext
+import com.wajam.commons.Logging
+
+/**
+ * Execution context decorator that saves the current tracing context when created and restore it every time
+ * it is executed.
+ */
+class TracingExecutionContext(ec: ExecutionContext) extends ExecutionContext with Logging {
+
+  val tracer = Tracer.currentTracer.get
+  val tracingContext = tracer.currentContext
+
+  def execute(runnable: Runnable) = {
+    ec.execute(new Runnable {
+      def run() {
+        tracer.trace(tracingContext) {
+          runnable.run()
+        }
+      }
+    })
+  }
+
+  def reportFailure(t: Throwable) = ec.reportFailure(t)
+}
+
