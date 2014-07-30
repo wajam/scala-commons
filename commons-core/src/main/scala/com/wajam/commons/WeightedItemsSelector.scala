1e0edee29d89ffc8e67bfe71968aeb8b792a938e
Jerome Gagnon <jerome@wajam.com>
2014-07-30 11:03:28 -0400

    WeightedItemsSelector implementation and basic tests

---
+++ commons-core/src/main/scala/com/wajam/commons/WeightedItemsSelector.scala
@@ -0,0 +1,23 @@
+package com.wajam.commons
+
+import scala.collection.immutable.Iterable
+import scala.util.Random
+
+/**
+ * Select next item T randomly based on each item weight.
+ * Scala adaptation of http://stackoverflow.com/questions/6409652/random-weighted-selection-java-framework
+ */
+class WeightedItemsSelector[T](weightedItems: Iterable[(Double, T)])(implicit random: Random = Random) {
+
+  private val (distributedMap, total) = weightedItems.foldLeft((new java.util.TreeMap[Double, T], 0.0))((acc, entry) => {
+    val (map, tot) = acc
+    val (weight, item) = entry
+    map.put(tot + weight, item)
+    (map, tot + weight)
+  })
+
+  /**
+   * Select next item T randomly
+   */
+  def next: T = distributedMap.ceilingEntry(random.nextDouble() * total).getValue
+}
---
+++ commons-core/src/test/scala/com/wajam/commons/TestWeightedItemsSelector.scala
@@ -0,0 +1,27 @@
+package com.wajam.commons
+
+import scala.util.Random
+
+import org.scalatest.{Matchers, FlatSpec}
+
+class TestWeightedItemsSelector extends FlatSpec
+with Matchers {
+
+  implicit def random = new Random(13)
+
+  "WeightedItemsSelector" should "select item according to weights" in {
+
+    val fields = List((1.0, "a"), (3.0, "b"), (1.0, "c"))
+
+    val selector = new WeightedItemsSelector[String](fields)
+
+    val selected = for (i <- 1 to 100) yield selector.next
+
+    val grouped = selected.groupBy(name => name)
+
+    grouped("a").size should be(21)
+    grouped("b").size should be(57)
+    grouped("c").size should be(22)
+  }
+
+}
